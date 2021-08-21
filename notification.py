a04c5f44b70b6b862af11d00e59fd5fbd900c402
TheShiveshNetwork <84664410+TheShiveshNetwork@users.noreply.github.com>
2021-08-21 19:58:58 +0530

    Add files via upload

---
+++ notification.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title="Alert Shivesh!!",
+            message="It's been one hour you are working on the pc... Take rest now!",
+            app_icon=r"C:\Users\HPindia\Pictures\Saved Pictures\Shivesh\Python Examples\py apps\icon.ico",)
+        time.sleep(60)
