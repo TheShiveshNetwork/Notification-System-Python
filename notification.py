14dad578b6062ea6bcd58ac7e2b2848cb899e9c0
TheShiveshNetwork <84664410+TheShiveshNetwork@users.noreply.github.com>
2021-08-21 19:59:35 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -4,7 +4,7 @@ from plyer import notification
 if __name__ == '__main__':
     while True:
         notification.notify(
-            title="Alert Shivesh!!",
+            title="Alert User!!",
             message="It's been one hour you are working on the pc... Take rest now!",
             app_icon=r"C:\Users\HPindia\Pictures\Saved Pictures\Shivesh\Python Examples\py apps\icon.ico",)
         time.sleep(60)
