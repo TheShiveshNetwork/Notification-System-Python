3b276bea5ea1c656cd050ed9fba176ad3fa4c641
Shivesh Tiwari <84664410+TheShiveshNetwork@users.noreply.github.com>
2021-09-04 17:30:50 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
         notification.notify(
             title="Alert User!!",
             message="It's been one hour you are working on the pc... Take rest now!",
-            app_icon=r"C:\Users\HPindia\Pictures\Saved Pictures\Shivesh\Python Examples\py apps\icon.ico",)
+            app_icon=r"./icon.ico",)
         time.sleep(60)
